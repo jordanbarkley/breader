ee2f13d4eb1ac35b487d0ac145321bf247542631
Jordan Barkley <jordan@charloe.com>
2018-12-03 22:55:02 -0500

    added functions to write to Firebase, fixed file titles

--- src/public/app.js
+++ src/public/app.js
@@ -4,34 +4,86 @@ console.log("app.js included");
 const USERNAME = "username";  
 
 // get relevant html elements
-const textFieldInput = document.querySelector("#inputValue");
 const paragraphOutput = document.querySelector("#outputValue");
-const buttonSave = document.querySelector("#buttonSaveValue");
-const buttonLoad = document.querySelector("#buttonLoadValue");
+const buttonTest = document.querySelector("#buttonTest");
 
 // Get a reference to the database service
 var database = firebase.database();
 
-// add a listener for click event on save button
-buttonSave.addEventListener("click", function() {
-    const textToSave = textFieldInput.value;
-    database.ref(USERNAME + "/test").set(textToSave);
+// add a listener for click event on test button
+buttonTest.addEventListener("click", function() {
+    paragraphOutput.innerHTML = breaderGetUID();
+    breaderSetBackgroundColor("#001122");
+    breaderSetCardColor("#334455");
+    breaderSetFontSize("#667788");
+    breaderAddText("The Itsy Bitsy Spider", "went up the water spout or some shit idk this is just for testing");
+    breaderSetWPM(500);
 });
 
-// add a listener for click event on save button
-buttonLoad.addEventListener("click", function() {
-    // get snapshot of /<username>/test
-    database.ref(USERNAME + "/test").on("value", function(snapshot) {
-        // update paragraph text with value
-        paragraphOutput.innerHTML = snapshot.val();
-    });
-});
+// Breader functions
 
-// get user details
-firebase.auth().onAuthStateChanged(function(user) {
+// string name ("The Itsy Bitsy Spider")
+// string text (<the itsy bits spider lyrics>)
+function breaderAddText(name, text) {
+    breaderSetX("texts/" + name, text);
+}
+
+// htmlColor color ("#RRGGBB")
+function breaderSetBackgroundColor(color) {
+    breaderSetX("backgroundColor", color);
+}
+
+// htmlColor color ("#RRGGBB")
+function breaderSetCardColor(color) {
+    breaderSetX("cardColor", color);
+}
+
+// htmlColor color ("#RRGGBB")
+function breaderSetFontColor(color) {
+    breaderSetX("fontColor", color);
+}
+
+// int size
+function breaderSetFontSize(size) {
+    breaderSetX("fontSize", size);
+}
+
+// int wpm
+function breaderSetWPM(wpm) {
+    breaderSetX("wpm", wpm);
+}
+
+// DO NOT USE THE FUNCTIONS BELOW OUTSIDE OF "app.js"
+function breaderGetUID() {
+    // get current user
+    var user = firebase.auth().currentUser;
+
+    // if user is logged in
     if (user) {
-        paragraphOutput.innerHTML = "user email " + user.email;
+        // return user uid
+        return user.uid;
+
+    // if user is not logged in
     } else {
-        paragraphOutput.innerHTML = "no user is logged in";
+        // return null
+        return null;
     }
-});
+}
+
+
+function breaderSetX(path, value) {
+    // get uid
+    var uid = breaderGetUID();
+
+    // if there's a valid uid
+    if (uid) {
+        // set /<uid>/wpm = wpm
+        database.ref(uid + "/" + path).set(value);
+
+    // otherwise
+    } else {
+        // log an error
+        console.log("Unable to get uid. Is the user logged in?");
+    }
+}
+
