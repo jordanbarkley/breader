a75f304c23e6e9138ee8816b8d7be0bd9a5024cb
Jordan Barkley <jordan@charloe.com>
2018-12-04 21:48:39 -0500

    added function to get query params

--- src/public/app.js
+++ src/public/app.js
@@ -119,6 +119,15 @@ function breaderGetWPM() {
     return breaderGetX("wpm");
 }
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
 // DO NOT USE THE FUNCTIONS BELOW OUTSIDE OF "app.js"
 function breaderGetUID() {
